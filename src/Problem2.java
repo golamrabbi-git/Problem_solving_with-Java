744669845636f0052ec0bc3dc76f270e5f611c7d
GRabbi467 <mgrabbi69@gmail.com>
2023-12-31 01:33:10 +0600

    solved problem2

--- src/Problem2.java
+++ src/Problem2.java
@@ -1,3 +1,15 @@
 public class Problem2 {
+    public static void main(String[] args) {
+        int[] arr =  {12, 13, 1, 10, 34, 10};
+        int n = arr.length;
+        int largest = 0;
+        for(int i=0 ; i<n ; i++){
+            if(arr[i]>largest){
+                largest = arr[i];
+            }
+        }
+        System.out.println(largest);
+
+    }
 
 }
