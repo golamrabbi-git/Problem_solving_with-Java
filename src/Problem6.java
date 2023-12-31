cc542da0c84fed26a53a21b8e2c5566111be960a
GRabbi467 <mgrabbi69@gmail.com>
2024-01-01 01:40:18 +0600

    problem6 solved

--- src/Problem6.java
+++ src/Problem6.java
@@ -1,10 +1,10 @@
 public class Problem6 {
     static boolean isPalindrome(String s,int l, int r){
-        char[] ch = s.toCharArray();
-        int n = ch.length;
+        int n = s.length();
         //Base Case
         if(l>= n/2) return true;
-        if(ch[l] != ch[r]) return false;
+
+        if(s.charAt(l)!= s.charAt(r)) return false;
         return isPalindrome(s,l+1,r-1);
     }
     public static void main(String[] args) {
