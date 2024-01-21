077fb19eea72901e8fdf7570e9c7162b46e6ebba
GRabbi467 <mgrabbi69@gmail.com>
2024-01-21 09:39:34 +0600

    problem11solved

--- src/Problem11.java
+++ src/Problem11.java
@@ -5,7 +5,6 @@ import java.util.Arrays;
 public class Problem11 {
     static ArrayList<Integer> find_duplicates(int[] arr, int n){
         ArrayList<Integer>ans = new ArrayList<Integer>();
-        //List<Integer> ans = new ArrayList<Integer>();
         Arrays.sort(arr);
         for(int i = 0 ; i < n-1 ; i++){
             if(arr[i] == arr[i+1]){
@@ -18,6 +17,5 @@ public class Problem11 {
     public static void main(String[] args) {
         int[] arr ={4,2,5,2,1,5};
         System.out.println( find_duplicates(arr,6));
-
     }
 }
